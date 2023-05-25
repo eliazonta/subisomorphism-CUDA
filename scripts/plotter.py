bdcf205efcd0d78954b61c1febeee1da0ac1c0c3
Elia Zonta <elia.zontaa@gmail.com>
2023-05-26 00:03:45 +0200

    parser WIP + small graph visualizer

---
+++ scripts/plotter.py
@@ -0,0 +1,15 @@
+# graph plotter with networkx 
+# unable to draw massive graphs 
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+g = nx.Graph()
+for row in open('data/gowalla.txt', 'r'):
+    lines = [i for i in row.split()]
+    g.add_edge(lines[0], lines[1])
+g.number_of_edges()
+nx.draw(g)
+plt.show()
+
+
