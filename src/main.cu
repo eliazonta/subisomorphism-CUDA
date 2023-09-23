8033f7c2786b7760805afee7a6641fea57aa21c3
Elia Zonta <elia.zontaa@gmail.com>
2023-09-23 16:39:57 +0200

    added properly the working flags

--- src/main.cu
+++ src/main.cu
@@ -7,7 +7,7 @@
 #include "../include/utils.cuh"
 #include "../include/parser.h"
 
-// #define USE_STRUCT
+#define USE_STRUCT
 __device__
 void isomorphism(struct COOGraph g1, struct COOGraph g2){
 
@@ -42,13 +42,22 @@ int main(int argc, char** argv) {
     readCOO_struct(file, &g2);
     #endif
 
-    #ifdef NO_STRUCT
-        int num_rows, num_cols, nnz;
-        int* rowidx, colidx;
-        double* weights;
+    #ifndef USE_STRUCT
+        // g1
+        int g1_num_rows, g1_num_cols, g1_nnz;
+        int* g1_rowidx, g1_colidx, g1_d_row_idx, g1_d_rowidx, g1_d_colidx;
+        double* g1_weights, g1_d_weights;
+
+        // g1
+        int g2_num_rows, g2_num_cols, g2_nnz;
+        int* g2_rowidx, g2_colidx, g2_d_row_idx, g2_d_rowidx, g2_d_colidx;
+        double* g2_weights, g2_d_weights;
+
+        readCOO(file, &g1_num_rows, &g1_num_cols, &g1_nnz, &g1_rowidx, &g1_colidx, &g1_weights);
+        readCOO(file, &g2_num_rows, &g2_num_cols, &g2_nnz, &g2_rowidx, &g2_colidx, &g2_weights);
     #endif
     // Initialize and allocate GPU memory for targetGraph, patternGraph, mapping, and other necessary data
-    
+    #ifdef USE_STRUCT
     // copy of stack data for g1
     d_g1.nnz = g1.nnz;
     d_g1.num_rows = g1.num_rows;
@@ -62,21 +71,36 @@ int main(int argc, char** argv) {
     d_g2.num_cols = g2.num_cols;
     // d_g2.rowidx = malloc_device<int>(d_g2.num_rows + 1);
     // d_g2.colidx = malloc_device<int>(d_g2.num_cols + 1);
+    #endif
 
     
     auto s = get_time();
-    
+
+    #ifdef USE_STRUCT
     // copy to device for g1
-    // copy_to_device(d_g1.rowidx, g1.rowidx, (g1.num_rows + 1) * sizeof(int));
-    // copy_to_device(d_g1.colidx, g1.colidx, (g1.num_cols + 1) * sizeof(int));
-    thrust::device_vector<int> d_g1.rowidx = g1.rowidx;
-    thrust::device_vector<int> d_g1.colidx = g1.colidx;
+    copy_to_device(d_g1.rowidx, g1.rowidx, (g1.num_rows + 1) * sizeof(int));
+    copy_to_device(d_g1.colidx, g1.colidx, (g1.num_cols + 1) * sizeof(int));
+    // thrust::device_vector<int> d_g1.rowidx = g1.rowidx; // has to be fixed, even just for fun :)
+    // thrust::device_vector<int> d_g1.colidx = g1.colidx;
     // copy to device for g2
-    // copy_to_device(d_g2.rowidx, g2.rowidx, (g2.num_rows + 1) * sizeof(int));
-    // copy_to_device(d_g2.colidx, g2.colidx, (g2.num_cols + 1) * sizeof(int));
-    thrust::device_vector<int> d_g2.rowidx = g2.rowidx;
-    thrust::device_vector<int> d_g2.colidx = g2.colidx;
-    
+    copy_to_device(d_g2.rowidx, g2.rowidx, (g2.num_rows + 1) * sizeof(int));
+    copy_to_device(d_g2.colidx, g2.colidx, (g2.num_cols + 1) * sizeof(int));
+    // thrust::device_vector<int> d_g2.rowidx = g2.rowidx;
+    // thrust::device_vector<int> d_g2.colidx = g2.colidx;
+    #endif
+
+    #ifndef USE_STRUCT
+    // copy to device for g1
+    thrust::device_vector<int> g1_d_rowidx = g1_rowidx;
+    thrust::device_vector<int> g1_d_colidx = g1_colidx;
+    thrust::device_vector<double> g1_d_weights = g1_weights;
+
+    // copy to device for g2
+    thrust::device_vector<int> g2_d_rowidx = g2_rowidx;
+    thrust::device_vector<int> g2_d_colidx = g2_colidx;
+    thrust::device_vector<double> g2_d_weights = g2_weights;
+        
+    #endif
     // elapsed
     auto time_H2D = get_time() - s;
 
@@ -87,13 +111,22 @@ int main(int argc, char** argv) {
 
     // Copy the results (e.g., valid mappings) from GPU to CPU
     
-
+    #ifdef USE_STRUCT
     // Free GPU memory and clean up
     cudaFree(d_g1.rowidx);
     cudaFree(d_g1.colidx);
 
     cudaFree(d_g2.rowidx);
     cudaFree(d_g2.colidx);
+    #endif
+
+    #ifndef USE_STRUCT
+    cudaFree(g1_d_rowidx);
+    cudaFree(g1_d_colidx);
+
+    cudaFree(g2_d_rowidx);
+    cudaFree(g2_d_colidx);
+    #endif
 
     return 0;
 }
